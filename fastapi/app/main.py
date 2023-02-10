2e017d69bf96dcb26ca5270750b3cf3d45704ea7
jsayurusadeepa <124349110+jsayurusadeepa@users.noreply.github.com>
2023-02-10 21:43:20 +0530

    Update main.py

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,5 +1,4 @@
 import requests
-import pyplot as plt
 from fastapi import FastAPI
 app = FastAPI()
 endpoint = "https://dc0c7b7d-7fbd-4b52-9d73-ad40481e374f-dev.e1-us-east-azure.choreoapis.dev/qvaq/ordsgeneratedapiforpl/1.0.0/stud"
@@ -7,7 +6,4 @@ headers = {'Authorization': 'Bearer eyJ4NXQiOiJZV1kxTm1Sa1pHSTVNekU0T0RCbFpEUmlN
 r = requests.get(endpoint, headers=headers)
 @app.get("/")
 def hello_world():
-    plt.plot([1, 2, 3, 4])
-    plt.ylabel('some numbers')
-    plt.show()
-    return {"Message":""}
+    return {"message": r.json()}
