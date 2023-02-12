f5b3f9e67df39920e73a163c263312f23feaaf44
jsayurusadeepa <124349110+jsayurusadeepa@users.noreply.github.com>
2023-02-12 23:14:30 +0530

    Update main.py

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -8,4 +8,4 @@ headers = {'Authorization':'Bearer eyJ4NXQiOiJZV1kxTm1Sa1pHSTVNekU0T0RCbFpEUmlNV
 r = requests.get(endpoint, headers=headers)
 @app.get("/")
 def hello_world():
-    return {"Message":r.json()['items']}
+    return {"Message":requests.post(endpoint1, r.json()['items'].json())}
